233b0871fb65ad764afd754348607fc6f2404d15
Vishal Patel <vishalpatel@Vishals-MacBook-Air.local>
2018-11-12 14:56:38 -0500

    code for fixing string error

--- console.py
+++ console.py
@@ -10,4 +10,5 @@ x = sb.run()
 ws = WSStatsBuilder()
 y = ws.run()
 
-session.query(WS_Winners).filter(WS_Winners.name == 'Boston Redsox')[0].team = session.query(WS_Winners).filter(Team.name='Boston Red Sox')[0]
+# To clean 'Boston redsox' error
+# session.query(WS_Winners).filter(WS_Winners.name == 'Boston Redsox')[0].team = session.query(WS_Winners).filter(Team.name='Boston Red Sox')[0]
