f1b7192f9320f0b331208576e4a3b7c70dcd6297
Scott.Bronder <scbronder12@gmail.com>
2018-11-14 17:10:17 -0500

    stats graph finished

--- dash_package/plots.py
+++ dash_package/plots.py
@@ -2,7 +2,7 @@ from dash_package.scraping import *
 import plotly as plt
 import plotly.graph_objs as go
 from flask_sqlalchemy import SQLAlchemy
-
+import pdb
 
 class DrawPlot:
     def __init__(self):
@@ -21,6 +21,46 @@ class DrawPlot:
                 )]
         return data
 
+    offensive_keys = [('Wins', 'wins'), ('Average Age', 'avg_age'), ('OPS', 'ops'), ('Home Runs', 'home_runs'),
+                    ('Batting Average', 'batting_avg'), ('Runs Scored', 'runs_scored')]
+    defensive_keys = [('Losses', 'losses'), ('Runs Allowed', 'runs_allowed'), ('Earned Runs', 'earned_runs'),
+                    ('ERA', 'era'), ('Strikeouts', 'strikeouts'), ('Fielding Perenctage', 'field_percent')]
+    all_keys = [('Wins', 'wins'), ('Average Age', 'avg_age'), ('OPS', 'ops'), ('Home Runs', 'home_runs'),
+              ('Batting Average', 'batting_avg'), ('Runs Scored', 'runs_scored'), ('Losses', 'losses'),
+              ('Runs Allowed', 'runs_allowed'), ('Earned Runs', 'earned_runs'), ('ERA', 'era'),
+              ('Strikeouts', 'strikeouts'), ('Fielding Perenctage', 'field_percent')]
+    off_keys = [x[1] for x in offensive_keys]
+    def_keys = [x[1] for x in defensive_keys]
+
+    def key_find(self, stat, year):
+
+        if stat in self.def_keys:
+            return self.ds.getDefensiveStatForEachTeamYear(stat, year)
+        else:
+            return self.os.getOffensiveStatForEachTeamYear(stat, year)[0]
+
+    def createScatterPlotForStatYear(self, year, stat1, stat2):
+        number_of_teams = self.key_find(stat1, year)
+        number_of_teams2 = self.key_find(stat2, year)
+        ws_winner = WS_Winners.query.filter(WS_Winners.year == year)[0].team.name
+        index = [(i, el.index(ws_winner)) for i, el in enumerate(number_of_teams) if ws_winner in el][0][0]
+
+        colors = ['rgba(204,204,204,1)' for team in number_of_teams]
+        colors[index] = 'rgba(222,45,38,0.8)'
+        traces = []
+        traces.append(go.Scatter(
+                x=[i[1] for i in number_of_teams],
+                y=[i[1] for i in number_of_teams2],
+                text=[i[0] for i in number_of_teams],
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'white'},
+                    'color': colors},
+                    ))
+        return traces
+
 
 class DefensiveStats:
     def __init__(self):
@@ -28,6 +68,7 @@ class DefensiveStats:
         self.team_names = self.cd.getTeamNames()
 
     def getTeamDefensiveStats(self, team_name):
+
         team_defensive_stats = Team.query.filter(Team.name == team_name)[0].defensive_stats
         return team_defensive_stats
 
@@ -42,6 +83,9 @@ class DefensiveStats:
     def getDefensiveStatForEachTeamYear(self, stat, year):
         year_stats = self.cd.getTeamsDefensiveStatsForYear(year)
         given_stat_year = [[team.team.name, getattr(team, stat)] for team in year_stats]
+        print('from defensive stats')
+        print(year_stats)
+        print(stat)
         return given_stat_year
 
 
